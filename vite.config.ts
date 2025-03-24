dec6cd39c7c99bcbb04dd94dbeb0131361b6f91b
kohdice <kohdice@gmail.com>
2025-03-24 22:15:58 +0900

    style: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { crx } from '@crxjs/vite-plugin';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-import tailwindcss from '@tailwindcss/vite';
-import manifest from './manifest.json';
+import tailwindcss from "@tailwindcss/vite";
+import manifest from "./manifest.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), crx({ manifest }), tailwindcss()],
-	build: {
-		rollupOptions: {
-			input: {
-				main: 'index.html',
-				background: 'src/background.ts',
-			},
-		},
-	},
+  plugins: [react(), crx({ manifest }), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        background: "src/background.ts",
+      },
+    },
+  },
 });
