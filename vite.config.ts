13171ae1ec522ab2838a2dbbac2d83531ae0c0af
kohdice <kohdice@gmail.com>
2025-03-24 23:32:19 +0900

    style: Modify quotes to single

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { crx } from "@crxjs/vite-plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { crx } from '@crxjs/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-import tailwindcss from "@tailwindcss/vite";
-import manifest from "./manifest.json";
+import tailwindcss from '@tailwindcss/vite';
+import manifest from './manifest.json';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,8 +11,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        background: "src/background.ts",
+        main: 'index.html',
+        background: 'src/background.ts',
       },
     },
   },
